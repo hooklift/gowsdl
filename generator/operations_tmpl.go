ecea1ae8171b1bcf5da35cf6e8ddd7a308408f84
Camilo Aguilar <camilo.aguilar@gmail.com>
2014-11-02 22:23:12 -0500

    Fixes formatting

--- generator/operations_tmpl.go
+++ generator/operations_tmpl.go
@@ -28,23 +28,23 @@ var opsTmpl = `
 		{{$output := findType .Output.Message}}
 
 		{{/*if ne $soapAction ""*/}}
-			{{if gt $faults 0}}
-			//
-			// Error can be either of the following types:
-			// {{range .Faults}}
-			//   - {{.Name}} {{.Doc}}{{end}}
-			//
-			{{end}}
-			{{if ne .Doc ""}}// {{.Doc}}{{end}}
-			func (service *{{$portType}}) {{makePublic .Name}} (request *{{$requestType}}) (*{{$output}}, error) {
-				response := &{{$output}}{}
-				err := service.client.Call("{{$soapAction}}", request, response)
-				if err != nil {
-					return nil, err
-				}
-
-				return response, nil
+		{{if gt $faults 0}}
+		//
+		// Error can be either of the following types:
+		// {{range .Faults}}
+		//   - {{.Name}} {{.Doc}}{{end}}
+		//
+		{{end}}
+		{{if ne .Doc ""}}// {{.Doc}}{{end}}
+		func (service *{{$portType}}) {{makePublic .Name}} (request *{{$requestType}}) (*{{$output}}, error) {
+			response := &{{$output}}{}
+			err := service.client.Call("{{$soapAction}}", request, response)
+			if err != nil {
+				return nil, err
 			}
+
+			return response, nil
+		}
 		{{/*end*/}}
 	{{end}}
 {{end}}
