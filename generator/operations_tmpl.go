03072d30854e2392a0c1ca55cb06c2d4d60f87bc
NotZippy <notzippy@gmail.com>
2015-03-17 09:11:36 -0700

    Fix for issue 21
    Signed-off-by: NotZippy <notzippy@gmail.com>

--- generator/operations_tmpl.go
+++ generator/operations_tmpl.go
@@ -32,7 +32,7 @@ var opsTmpl = `
 		// Error can be either of the following types:
 		// {{range .Faults}}
 		//   - {{.Name}} {{.Doc}}{{end}}{{end}}
-		{{if ne .Doc ""}}// {{.Doc}}{{end}}
+		{{if ne .Doc ""}}/* {{.Doc}} */{{end}}
 		func (service *{{$portType}}) {{makePublic .Name | replaceReservedWords}} ({{if ne $requestType ""}}request *{{$requestType}}{{end}}) (*{{$responseType}}, error) {
 			response := &{{$responseType}}{}
 			err := service.client.Call("{{$soapAction}}", {{if ne $requestType ""}}request{{else}}nil{{end}}, response)
