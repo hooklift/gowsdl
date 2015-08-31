ce626ace5feb4e066d0221029326d6b5556eca9c
Camilo Aguilar <camilo.aguilar@gmail.com>
2015-08-31 12:29:49 -0400

    Removes broken examples

--- examples/usda-awdb/main.go
+++ examples/usda-awdb/main.go
@@ -11,15 +11,15 @@ import (
 func main() {
 	gowsdl.Log.SetHandler(log15.StreamHandler(os.Stdout, log15.TerminalFormat()))
 
-	service := NewAwdbWebService("http://www.wcc.nrcs.usda.gov/awdbWebService/services", false)
-	amIthere, err := service.AreYouThere(&areYouThere{})
+	service := NewAwdbWebService("http://www.wcc.nrcs.usda.gov/awdbWebService/services", false, nil)
+	amIthere, err := service.AreYouThere(&AreYouThere{})
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("Alive?: %t\n", amIthere.Return_)
 
-	request := &getStations{NetworkCds: []string{"SNTL"}, LogicalAnd: true}
+	request := &GetStations{NetworkCds: []string{"SNTL"}, LogicalAnd: true}
 	stations, err := service.GetStations(request)
 	if err != nil {
 		fmt.Printf("\n->%s\n", err.(*gowsdl.SoapFault).Faultstring)
