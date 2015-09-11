ed65775d010e73d7d5908304e2b2550bb6dd8947
Mark Sanborn <sanbornm@gmail.com>
2015-09-10 22:37:47 -0700

    dead code

--- generator/wsdl_test.go
+++ generator/wsdl_test.go
@@ -20,14 +20,4 @@ func TestUnmarshal(t *testing.T) {
 	if err != nil {
 		t.Errorf("incorrect result\ngot:  %#v\nwant: %#v", err, nil)
 	}
-
-	// for _, pt := range v.PortTypes {
-	// 	t.Logf("PortType name: %s\n", pt.Name)
-	// 	for _, o := range pt.Operations {
-	// 		t.Logf("Operation: %s", o.Name)
-	// 	}
-	// 	t.Logf("Total ops: %d\n", len(pt.Operations))
-	// }
-
-	// t.Logf("%#v\n", v.Types.Schema[0].Includes)
 }
