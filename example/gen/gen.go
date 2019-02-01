27d39204a596f0cf03410812d4f57f57e8c703af
Iszak Bryan <38895+iszak@users.noreply.github.com>
2019-02-01 16:57:59 +0000

    Updated tests

--- example/gen/gen.go
+++ example/gen/gen.go
@@ -1,6 +1,7 @@
 package gen
 
 import (
+	"context"
 	"encoding/xml"
 	"time"
 
@@ -39,7 +40,7 @@ func NewStockQuotePortType(client *soap.Client) StockQuotePortType {
 
 func (service *stockQuotePortType) GetLastTradePrice(request *TradePriceRequest) (*TradePrice, error) {
 	response := new(TradePrice)
-	err := service.client.Call("http://example.com/GetLastTradePrice", request, response)
+	err := service.client.Call(context.Background(), "http://example.com/GetLastTradePrice", request, response)
 	if err != nil {
 		return nil, err
 	}
