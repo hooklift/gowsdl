0a43678a1a3932aaa699437fbc5fe35bd374491f
Iszak Bryan <38895+iszak@users.noreply.github.com>
2019-02-22 16:52:32 +0000

    Add context to interface

--- operations_tmpl.go
+++ operations_tmpl.go
@@ -24,6 +24,8 @@ var opsTmpl = `
 			{{if ne .Doc ""}}/* {{.Doc}} */{{end}}
 			{{makePublic .Name | replaceReservedWords}} ({{if ne $requestType ""}}request *{{$requestType}}{{end}}) ({{if ne $responseType ""}}*{{$responseType}}, {{end}}error)
 			{{/*end*/}}
+			{{makePublic .Name | replaceReservedWords}}Context (ctx context.Context, {{if ne $requestType ""}}request *{{$requestType}}{{end}}) ({{if ne $responseType ""}}*{{$responseType}}, {{end}}error)
+			{{/*end*/}}
 		{{end}}
 	}
 
