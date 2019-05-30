8e7a0fa780ee245be8141cfcbc087bc7bd9cbc9d
Ori Moisis <ori.moisis@salto.io>
2019-05-30 10:18:52 +0300

    force empty soap action to exist in the generated request

--- operations_tmpl.go
+++ operations_tmpl.go
@@ -45,7 +45,7 @@ var opsTmpl = `
 		{{$responseType := findType .Output.Message | replaceReservedWords | makePublic}}
 		func (service *{{$privateType}}) {{makePublic .Name | replaceReservedWords}}Context (ctx context.Context, {{if ne $requestType ""}}request *{{$requestType}}{{end}}) ({{if ne $responseType ""}}*{{$responseType}}, {{end}}error) {
 			{{if ne $responseType ""}}response := new({{$responseType}}){{end}}
-			err := service.client.CallContext(ctx, "{{$soapAction}}", {{if ne $requestType ""}}request{{else}}nil{{end}}, {{if ne $responseType ""}}response{{else}}struct{}{}{{end}})
+			err := service.client.CallContext(ctx, "{{if ne $soapAction ""}}{{$soapAction}}{{else}}''{{end}}", {{if ne $requestType ""}}request{{else}}nil{{end}}, {{if ne $responseType ""}}response{{else}}struct{}{}{{end}})
 			if err != nil {
 				return {{if ne $responseType ""}}nil, {{end}}err
 			}
