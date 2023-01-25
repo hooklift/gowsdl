06dbac3f97b41598e63f98a8e331f433fee480d3
Dewayne Richardson <drichardson@ascendsoftware.com>
2023-01-25 10:39:24 -0700

    updated the packages

--- example/gen/gen.go
+++ example/gen/gen.go
@@ -4,7 +4,7 @@ import (
 	"encoding/xml"
 	"time"
 
-	"github.com/hooklift/gowsdl/soap"
+	"github.com/ascendsoftware/gowsdl/soap"
 )
 
 // against "unused imports"
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/hooklift/gowsdl
+module github.com/ascendsoftware/gowsdl
 
 go 1.15
 
